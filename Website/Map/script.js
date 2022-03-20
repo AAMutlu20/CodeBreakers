2500f85bb92a0c08170035b85f77dcb9b10ef345
ESBogdanov20 <85444197+ESBogdanov20@users.noreply.github.com>
2022-03-20 11:11:54 +0200

    map changes

--- Website/Map/script.js
+++ Website/Map/script.js
@@ -1,5 +1,7 @@
-var map = L.map('mapid').setView([47.58186, 13.26485], 7.5);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
-}).addTo(map);
+$(document).ready(function(){
+    tippy('.tippy', {
+      theme: 'light',
+      size: 'big',
+      arrow: true
+    })
+});
